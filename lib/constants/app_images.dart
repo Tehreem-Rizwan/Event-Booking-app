71917500162e0d62dff596bbbd7b7583a9a640b1
Tehreem Rizwan <its.tehreem2830@gmail.com>
2025-02-22 22:48:20 +0500

    updated

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -2,5 +2,44 @@
 class Assets {
   Assets._();
   
+  /// Assets for imagesConfetti
+  /// assets/images/confetti.png
+  static const String imagesConfetti = "assets/images/confetti.png";
+
+  /// Assets for imagesDish
+  /// assets/images/dish.png
+  static const String imagesDish = "assets/images/dish.png";
+
+  /// Assets for imagesEvent
+  /// assets/images/event.jpg
+  static const String imagesEvent = "assets/images/event.jpg";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.png
+  static const String imagesGoogle = "assets/images/google.png";
+
+  /// Assets for imagesMusical
+  /// assets/images/musical.png
+  static const String imagesMusical = "assets/images/musical.png";
+
+  /// Assets for imagesOnboarding
+  /// assets/images/onboarding.png
+  static const String imagesOnboarding = "assets/images/onboarding.png";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.png
+  static const String imagesProfile = "assets/images/profile.png";
+
+  /// Assets for imagesTicket
+  /// assets/images/ticket.png
+  static const String imagesTicket = "assets/images/ticket.png";
+
+  /// Assets for imagesTshirt
+  /// assets/images/tshirt.png
+  static const String imagesTshirt = "assets/images/tshirt.png";
+
+  /// Assets for imagesUp
+  /// assets/images/up.png
+  static const String imagesUp = "assets/images/up.png";
 }
 
--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:event_booking_app/constants/app_colors.dart';
+import 'package:event_booking_app/constants/app_images.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.only(top: 50, left: 20.0),
+        padding: const EdgeInsets.only(top: 50, left: 20.0, right: 20),
         width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
@@ -21,7 +22,7 @@ class _HomePageState extends State<HomePage> {
           Color(0xfff1f3ff),
           kSecondaryColor,
         ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-        child: const Column(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
@@ -46,6 +47,168 @@ class _HomePageState extends State<HomePage> {
                   color: kPrimaryColor,
                   fontSize: 25),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "There are 20 events\n around your location",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff6351ec),
+                  fontSize: 25),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: kSecondaryColor,
+                  borderRadius: BorderRadius.circular(10)),
+              child: const TextField(
+                decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.search_outlined),
+                    hintText: "Search a Location",
+                    border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      width: 130,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: kSecondaryColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            Assets.imagesMusical,
+                            height: 30,
+                            width: 30,
+                            fit: BoxFit.cover,
+                          ),
+                          Text(
+                            "Music",
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 130,
+                  ),
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      width: 130,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: kSecondaryColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            Assets.imagesTshirt,
+                            height: 30,
+                            width: 30,
+                            fit: BoxFit.cover,
+                          ),
+                          Text(
+                            "Clothing",
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 130,
+                  ),
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      width: 130,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: kSecondaryColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            Assets.imagesConfetti,
+                            height: 30,
+                            width: 30,
+                            fit: BoxFit.cover,
+                          ),
+                          Text(
+                            "Festival",
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 130,
+                  ),
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      width: 130,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: kSecondaryColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            Assets.imagesDish,
+                            height: 30,
+                            width: 30,
+                            fit: BoxFit.cover,
+                          ),
+                          Text(
+                            "Food",
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
